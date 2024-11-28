8f303f95fb321768cd54fbb74f9c9ee7aa1ef90c
Zain zafar <zainzafar@Zains-MacBook-Air.local>
2024-11-28 00:35:48 -0500

    ProtectedRoute component build created

--- uni-manager/src/App.jsx
+++ uni-manager/src/App.jsx
@@ -2,24 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
-
-
 import Welcome from './Components/WelcomePageComponents/WelcomeScreen';
 import Login from './Components/LoginComponents/Login';
 import CreateUser from './Components/CreateAccountComponenets/CreateAccount';
 import HomePage from './Components/HomePageComponents/HomePage';
+import ProtectedRoute from './Components/Ultilities/ProtectedRoute';
 
 //import './App.css';
 import axios from "axios";
 const serverURL = "http://localhost:3000";
 
 // Login authentication state
-
-
-
-
-
-
 
 function App() {
   
@@ -58,7 +51,7 @@ function App() {
                 <Route path="/" element={<Welcome />}/>
                 <Route path="/login" element={<Login login={login} setUsername={setUsername} username={username}/>}/>
                 <Route path="/create-account" element={<CreateUser />}/>
-                <Route path="/home-page" element={ProtectedRoute(isAuthenticated)}/>
+                <Route path="/home-page" element={<ProtectedRoute login={<Login login={login} setUsername={setUsername} username={username}/>}  passRoute={<HomePage logout={logout} username={username}/>}  />}/>
 
             </Routes>
         </Router>
